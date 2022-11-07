3049ec81e2b95ba776c6fbc68f45ed46e93fa6f3
Trefania <tarirotrefania@gmail.com>
2022-11-07 12:29:07 -0800

    bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,12 +12,13 @@ void bubble_sort(int *array, size_t size)
 
 	for (a = 0; a < size; a++)
 	{
-		for (b =0; b < (size-1); b++)
+		for (b = 0; b < (size - 1); b++)
 		{
 			if (array[b] > array[b + 1])
 			{
 				int temp = array[b];
-				array[b] = array[b +1];
+
+				array[b] = array[b + 1];
 				array[b + 1] = temp;
 				print_array(array, size);
 			}
