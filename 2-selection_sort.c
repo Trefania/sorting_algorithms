adfddb863e57fe572702efaca170c8604a67f425
Trefania <tarirotrefania@gmail.com>
2022-11-08 14:47:01 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - Function that swaps two values
+ *
+ * @f: Fisrt value
+ * @s: Second value
+ * Return: 0
+ */
+void swap(int *f, int *s)
+{
+        int tmp;
+
+        tmp = *s;
+        *s = *f;
+        *f = tmp;
+
+/**
  * selection_sort - function that sort selction
  * @array: array
  * @size: size of the array
@@ -25,20 +40,4 @@ void selection_sort(int *array, size_t size)
 			print_array(array, size);
 		}
 	}
-}
-
-/**
- * swap - Function that swaps two values
- *
- * @f: Fisrt value
- * @s: Second value
- * Return: 0
- */
-void swap(int *f, int *s)
-{
-	int tmp;
-
-	tmp = *s;
-	*s = *f;
-	*f = tmp;
 }
