a73d761442b97e31cdfebf7d5af54462b267387f
Trefania <tarirotrefania@gmail.com>
2022-11-08 13:44:33 -0800

    insertion_sort

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void print_list(const listint_t *list);
 /* sorting algorithm */ 
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
 #endif  /* sort_h */
